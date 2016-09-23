828e3e7c03e90dbcda9ae8943efa1097a578a753
Guest User <guest@bimsb-tr01l.bimsb.net>
2016-09-23 10:25:50 +0200

    fixed exprs log values, added var filtering

--- capstone.R
+++ capstone.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(randomForest)
 library(ggfortify)
 library(cowplot)
+library(matrixStats)
 
 # Methylation
 meth=readRDS("/data/compgen2016/day10_projectDay/methylation.rds")
@@ -23,8 +24,11 @@ head(pat)
 pat[1:5,,drop=FALSE]
 
 # PCA
+expr <- log2(exp$dat)
+expr <- expr[rowSums(expr[, -1])>0, ]
+
 meth.pca <- prcomp(t(meth$dat))
-expr.pca <- prcomp(t(exp$dat))
+expr.pca <- prcomp(t(expr))
 cnv.pca <- prcomp(t(cna$dat))
   
 title <- ggdraw() + draw_label("Methylation data PCA", fontface='bold')
@@ -42,7 +46,24 @@ save_plot("plots/PCA.pdf", pl, base_width=15, nrow=1)
 
 
 # Most variable data
-#library(matrixStats)
-#vars <- rowVars(golub)
-#N <- 5
-#ndx <- order(vars, decreasing = T)[1:N]
+library(matrixStats)
+
+meth.vars <- rowVars(meth$dat)
+expr.vars <- rowVars(expr)
+cnv.vars <- rowVars(as.matrix(cna$dat))
+
+meth.varmean <- mean(meth.vars)
+expr.varmean <- mean(expr.vars)
+cnv.varmean <- mean(cnv.vars)
+
+cnv.N <- 50
+meth.N <- 50
+expr.N <- 50
+meth.ndx <- order(meth.vars, decreasing = T)[1:meth.N]
+expr.ndx <- order(expr.vars, decreasing = T)[1:expr.N]
+cnv.ndx <- order(cnv.vars, decreasing = T)[1:cnv.N]
+
+meth.f <- meth$dat[meth.ndx, ]
+expr.f <- expr[expr.ndx, ]
+cnv.f <- cna$dat[cnv.ndx, ]
+
